79dddd2b878c18513d326a7a0963c6cdd478b4f8
Oleg Skuibida <oleg@ongoza.com>
2021-10-02 19:38:48 +0300

    Add files via upload

---
+++ clockCarSettings.js
@@ -0,0 +1,12 @@
+        //var car_img = "sedan_red_100.png";		
+        //var car_img = "universal_white_100.png";
+	var car_img = "universal_white_100_1.png";
+	//var car_img = "universal_yellow_100_1.png";
+	//var car_img = "universal_blue_100_1.png";
+        var background_color = 'Black';
+        var car_fade_timer = 500;
+	// car_speed 1 - 20
+        var car_speed = 3;
+        //var car_speed_out = car_speed*2;
+        var test_mode = true; // 4 or 6 digit
+        var out_mode = 'drag'; // 'fadeOut'
